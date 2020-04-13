b10e8a9566e01896636d66227c84691d3a1c22c9
DevStores <qpwoeiruty1871@gmail.com>
2020-04-13 16:30:39 -0300

    first commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -50,7 +50,7 @@ function Layout({ children }) {
                   </ul>
                   <form action={data.site.siteMetadata.github}>
                     <button type="submit" id="navAction" 
-                            className="mx-auto lg:mx-0 hover:underline font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 gradient text-white">Get Sources</button>
+                            className="mx-auto lg:mx-0 hover:underline font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 gradient text-white">Whatsapp</button>
                   </form>
 
                 </div>
